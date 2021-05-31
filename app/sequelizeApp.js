c233a9d4a381065637161999b6843a84863fc693
Mikael <mikael.r.simoes@hotmail.com>
2021-05-30 21:53:51 -0300

    chore: Add form

--- app/sequelizeApp.js
+++ app/sequelizeApp.js
@@ -1,37 +1,37 @@
-//Importing Sequelize
-import Sequelize from 'sequelize';
+// //Importing Sequelize
+// import Sequelize from 'sequelize';
 
-//Instantiating Sequelize
-const sequelize = new Sequelize('node_db', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+// //Instantiating Sequelize
+// const sequelize = new Sequelize('node_db', 'root', '', {
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
 
-//Establishing connection to database
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
+// //Establishing connection to database
+// try {
+//   await sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
 
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+// }
 
-//Creating the Payment table reference
-const Payment = sequelize.define('payments', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  value: {
-    type: Sequelize.DOUBLE
-  }
-});
+// //Creating the Payment table reference
+// const Payment = sequelize.define('payments', {
+//   name: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   value: {
+//     type: Sequelize.DOUBLE
+//   }
+// });
 
-//Creating Payment Table
-Payments.sync({force: true});
+// //Creating Payment Table
+// Payments.sync({force: true});
 
-//Inserting data on Payment table
-Payment.create({
-    name: 'Energy',
-    value: 400
-});
+// //Inserting data on Payment table
+// Payment.create({
+//     name: 'Energy',
+//     value: 400
+// });
